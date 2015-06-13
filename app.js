cd293d872852ef1c03d84fd462e9d8fc6c30eb15
ingvirafn <ingvirafn@ingvirafn.com>
2015-06-13 09:27:11 +0000

    add nodejs

---
+++ app.js
@@ -0,0 +1,26 @@
+﻿var debug = debug || console.log;
+
+// Setup server
+var express = require('express');
+var app = express();
+var SERVERPORT = process.env.PORT || 3000;
+
+var secretStuff = process.env.Secret || 'No secret!';
+
+// Statics
+app.use(express.static('public'));
+
+app.get('/secret', function(req,res) { return res.json({ secret: secretStuff }); });
+
+// ELSE!
+app.use(function(req, res, next){
+	res.writeHead(302, {
+	  'Location': '/'
+	});
+	res.end();
+});
+
+// Start the app
+var server = app.listen(SERVERPORT, function() {
+  debug('Express server listening on port ' + server.address().port);
+});
